6d0297377452cab09836a48abe61ef667bf44d55
EMe-U <e.umurerwa@alustudent.com>
2025-02-13 23:58:12 -0800

    yes

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,88 +1,69 @@
 #!/usr/bin/python3
-"""
-Fabric script that creates and distributes an archive to web servers.
-"""
+import re
+from time import strftime
+from fabric.context_managers import cd
+from fabric.api import env, put, sudo, local
+from os.path import join, exists, splitext
 
-from fabric import Connection, task
-from datetime import datetime
-import os
-
-# List of servers
-servers = ['54.91.80.128', '54.90.94.220']
+env.hosts = ["54.91.80.128", "54.90.94.220"]
 
 
 def do_pack():
     """
-    Generates a .tgz archive from the contents of the web_static folder.
+    Generates a .tgz file from the contents of the web_static folder
 
     Returns:
-        str: The path to the created archive if successful, otherwise None.
+        str: The file path of the generated .tgz file if successful else None.
     """
-    time_stamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
-    os.system("mkdir -p versions")
-    archive_path = f"versions/web_static_{time_stamp}.tgz"
-    os.system(f"tar -cvzf {archive_path} web_static")
 
-    if os.path.exists(archive_path):
-        return archive_path
-    return None
+    date_time = strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(date_time)
+    try:
+        local("mkdir -p versions")
+        local("tar -zcvf {} web_static".format(file_name))
+        return file_name
+    except Exception as err:
+        return None
 
 
 def do_deploy(archive_path):
     """
-    Deploys the archive to the web servers.
-    
+    Deploy a compressed archive to a remote server.
     Args:
-        archive_path (str): The path to the archive file.
-
+        archive_path (str): The path to the compressed archive.
     Returns:
-        bool: True if successful, False otherwise.
+        bool: True if the deployment is successful, False otherwise.
     """
-    if not os.path.exists(archive_path):
+
+    if not exists(archive_path):
         return False
 
     try:
-        file_name = archive_path.split("/")[-1]
-        name = file_name.split(".")[0]
-        remote_path = f"/data/web_static/releases/{name}"
+        put(archive_path, "/tmp/")
+        file_name = re.search(r'[^/]+$', archive_path).group(0)
+        deploy_path = join("/data/web_static/releases/",
+                           splitext(file_name)[0])
+        sudo("mkdir -p {}".format(deploy_path))
 
-        for server in servers:
-            conn = Connection(host=server, user="ubuntu")
+        sudo("tar -xzf /tmp/{} -C {}".format(file_name, deploy_path))
 
-            # Upload archive
-            conn.put(archive_path, "/tmp/")
+        with cd(deploy_path):
+            sudo("mv web_static/* .")
+            sudo("rm -rf web_static")
 
-            # Create release folder
-            conn.run(f"mkdir -p {remote_path}")
+        sudo("rm /tmp/{}".format(file_name))
+        sudo("rm -rf /data/web_static/current")
 
-            # Unpack archive
-            conn.run(f"tar -xzf /tmp/{file_name} -C {remote_path}")
-
-            # Remove archive from /tmp
-            conn.run(f"rm /tmp/{file_name}")
-
-            # Move files to proper location
-            conn.run(f"mv {remote_path}/web_static/* {remote_path}/")
-
-            # Remove empty directory
-            conn.run(f"rm -rf {remote_path}/web_static")
-
-            # Update symlink
-            conn.run("rm -rf /data/web_static/current")
-            conn.run(f"ln -s {remote_path} /data/web_static/current")
-
-        return True
-    except Exception as e:
-        print(f"Error: {e}")
+        sudo('ln -sf {} /data/web_static/current'.format(deploy_path))
+    except Exception as err:
         return False
+
+    return True
 
 
 def deploy():
     """
-    Creates and distributes an archive to web servers.
-    
-    Returns:
-        bool: True if successful, False otherwise.
+    Deploys the web_static content to the web servers.
     """
     archive_path = do_pack()
     if not archive_path:
