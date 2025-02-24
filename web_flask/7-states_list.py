ebab69022153296d15932cfd1c5f9285a32a8a02
EMe-U <e.umurerwa@alustudent.com>
2025-02-24 01:24:16 -0800

    yes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-
-"""Starts a Flask web application"""
-
+""" a script that starts a Flask web application and list states """
+from flask import Flask, render_template
 from models import storage
-from models.state import State
-from flask import Flask
-from flask import render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def states():
-    """Comment"""
-    return render_template('7-states_list.html',
-                           states=storage.all('State').values())
+def lists_States():
+    """ display states in HTML page """
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Remove the current SQLAlchemy Session"""
+def teardown(exc):
+    """ remove the current SQLAlchemy Session """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
